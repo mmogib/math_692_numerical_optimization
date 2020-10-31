49bf3d19ef66f200f05a6fe635aba337fbd2c793
Mohammed Alshahrani <mmogib@gmail.com>
2020-10-31 14:14:54 +0300

    finished chapter 13

--- Dwell.m
+++
@@ -1,9 +0,0 @@
-function D = Dwell(f,x)
-%DWELL Summary of this function goes here
-%   Detailed explanation goes here
-n = numel(x);
-e=1e-30;
-p= 1:n;
-df = arrayfun(@(j) imag((f(x+(e*1i)*(p==j)))/e),p);
-D = [f(x) df]';
-end
